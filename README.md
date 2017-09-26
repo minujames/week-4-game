6655ea0df6284367295ba9f94606bc963245d380
Minu James <minujames@minus-mbp.gateway.pace.com>
2017-09-25 23:24:20 -0700

    UI modified

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# week-4-game
+# Star war RPG
+This is a star war role playing game, in which you can select your character and a defender and fight against each other. The player who looses all the health points first will loose the game.
+
+## Built With
+* [JQuery](https://jquery.com/)
+* [Bootstrap 3.3.6](http://bootstrapdocs.com/v3.3.6/docs/)
+* [CSS](https://www.w3schools.com/css/)
+
+## Author
+[Minu James](https://minujames.github.io/)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,136 +1,205 @@
 $(document).ready(function(){
 
-  function Player (name, healthPoints, attackPower, counterAttackPower) {
+  // Player constructor function to create player objects
+  function Player (name, healthPoints, attackPower, counterAttackPower, image) {
       this.name = name;
       this.healthPoints = healthPoints;
       this.attackPower = attackPower;
       this.counterAttackPower = counterAttackPower;
       this.earnedAttackPower = attackPower;
-      // this.image = image; 
+      this.image = image;
   }
    
+  // Adding methods to Player class 
   $.extend(Player.prototype, {
-    getInfo: function() {
-        return this.name + ' ' + this.healthPoints + ' ' + this.attackPower +
-          ' ' + this.counterAttackPower + ' ' + this.earnedAttackPower;
-    },
 
     attack: function(defender) {
-      // add hasOwnProperty
       defender.healthPoints = defender.healthPoints - this.earnedAttackPower;
       this.healthPoints = this.healthPoints - defender.counterAttackPower;
-      this.earnedAttackPower = this.earnedAttackPower + this.attackPower;
+    },
+
+    earnAttackPower: function(){
+      this.earnedAttackPower += this.attackPower;
     }
   });
 
+  // Game object which contains all the game logic and DOM manipulation
   var game = {
+    // For easy look up players are kept in a dictionary, instead of array.
+    // Player name is the key, which is considered as the unique key.
     players : {},
-    hero: '',
-    defender: '',
-    enemies: '',
+    hero: null,
+    defender: null,
+    enemies: null,
 
     initialize: function(){
       this.reset();
-      displayPlayers();
     },
 
     reset: function(){
-      var player1 = new Player("P1 Minu", 100, 6, 10);
-      var player2 = new Player("P2 Jayesh", 150, 10, 20);
-      var player3 = new Player("P3 Nidhi", 180, 15, 25);
-      var player4 = new Player("P4 Anoop", 120, 8, 15);
-
+      var player1 = new Player("Luke Skywalker", 100, 6, 10, "luke.jpg");
+      var player2 = new Player("Darth Maul", 120, 8, 15, "darth-maul.jpg");
+      var player3 = new Player("Darth Vader", 150, 10, 20, "darth-vader.jpg");
+      var player4 = new Player("Yoda", 180, 15, 25, "yoda.jpg");
+      
       this.players[player1.name] = player1;
       this.players[player2.name] = player2;
       this.players[player3.name] = player3;
       this.players[player4.name] = player4;
-      // console.log(this.players);
+
+      this.enemies = this.players;
+
+      this.displayPlayers();
+      this.toggleRestartButton();
+      this.displayMessage("");
+      this.disableAttackButton(true);
+    },
+
+    displayPlayers: function(){
+      for (var playerName in this.players) {     
+        var playerObj = this.players[playerName];
+
+        var player = $("<div>");
+        player.attr("player-name", playerName);
+        player.append("<section id='name'>" + playerName + "</section>");
+        player.addClass("character");
+        player.addClass("player");
+
+        var playerImg = $("<img>");
+        playerImg.attr("src", "assets/images/" + playerObj.image);
+        playerImg.addClass("player-image");
+        player.append(playerImg);
+        
+        player.append("<section id='hp'>" + playerObj.healthPoints + "</section>");
+        $("#your-character").append(player);
+      }
+    },
+
+    setHeroAndEnemies: function(player){
+      if(this.hero !== null){
+        this.displayMessage("You have already selected your character!");
+      }
+      else{
+        var heroName = $(player).attr("player-name");
+        this.hero = this.players[heroName];
+        console.log("Your character: " + this.hero.name);
+        delete this.enemies[heroName];
+
+
+        // Iterating through each player and giving them class enemy if that player is not hero.
+        // Setting the class hero for the selected player.
+        $("#your-character > div").each(function () {
+          var playerName = $(this).attr("player-name");
+          $(this).removeClass("player");
+
+          if(playerName === heroName){
+            $(this).addClass("hero");
+          }
+          else{
+            $(this).addClass("enemy");
+            $("#enemies").append(this);
+          }
+        });
+      }
+    },
+
+    setDefender: function(player){
+      if(this.defender !== null){
+        this.displayMessage("You have already selected a defender");
+      }
+      else{
+        this.disableAttackButton(false);
+        // Removing the existing defender and assigning the new defender.
+        $(".defender").remove();
+        var defender = $(player).attr("player-name");
+        this.defender = this.players[defender];
+        delete this.enemies[defender];
+        console.log("Defender: " + this.defender.name);
+        console.log("enemies list: " + this.enemies)
+
+        $(player).addClass("defender");
+        $(player).removeClass("enemy");
+        $("#defender").append(player);
+        this.displayMessage("Start fight by pressing Attack");
+      }
+    },
+
+    attack: function(){
+      var message = "";
+
+      this.hero.attack(this.defender);
+      this.updateHealthPoints();
+      
+      if(this.hero.healthPoints <= 0 || this.defender.healthPoints <= 0)
+      {
+        var isHeroWinner = this.hero.healthPoints > this.defender.healthPoints ? true : false;
+        if(isHeroWinner){
+          if(Object.keys(this.enemies).length === 0){
+            message = "You Won! Press Restart to play again";
+            this.toggleRestartButton();
+          }
+          else{
+            message = "You have defeated " + this.defender.name + ", you can choose to fight another enemy."
+            this.defender = null;
+          }
+        }
+        else{
+          message = "You been defeated... GAME OVER! Press Restart to play again!"
+          this.toggleRestartButton();
+        }
+        this.disableAttackButton(true);
+      }
+      else{ 
+        message = "You attacked " + this.defender.name + " for " + this.hero.earnedAttackPower 
+        + " damage." + this.defender.name + " attacked you back for " + this.defender.counterAttackPower 
+        + " damage.";
+      }
+
+      this.hero.earnAttackPower();
+      this.displayMessage(message);
+    },
+
+    clearCharacters: function(){
+      $( ".character" ).remove();
+    },
+
+    toggleRestartButton: function(){
+      $("#button-restart").toggle();
+    },
+
+    updateHealthPoints: function(){
+      $(".hero").children("#hp").text(this.hero.healthPoints);
+      $(".defender").children("#hp").text(this.defender.healthPoints);
+    },
+
+    displayMessage: function(message){
+      $(".message").text(message);
+    },
+
+    disableAttackButton: function(status){
+      $("#button-attack").attr("disabled", status);
     }
+
   };
 
   game.initialize();
 
-  function displayPlayers()
-  {
-
-    for (var playerName in game.players) {     
-      var playerObj = game.players[playerName];
-      // console.log(playerName, playerObj);
-
-      var player = $("<div>");
-      player.attr("player-name", playerName);
-      player.append("<section id='name'>" + playerName + "</section>");
-      player.addClass("character");
-      player.addClass("player");
-
-      var playerImg = $("<img>");
-      playerImg.attr("src", "assets/images/luke.jpg");
-      playerImg.addClass("player-image");
-      player.append(playerImg);
-      
-      player.append("<section id='hp'>" + playerObj.healthPoints + "</section>");
-      $("#your-character").append(player);
-    }
-
-  }
-
   // Registering event on static parent
   $("#your-character").on("click", ".player", function(){
-    console.log("hero: " + this);
-
-    if(game.hero !== '')
-    {
-      console.log("You have already selected a hero!")
-      console.log(this);
-    }
-    else
-    {
-      var hero = $(this).attr("player-name");
-      game.hero = game.players[hero];
-      console.log(hero);
-
-      // immediate child selector
-      $("#your-character > div").each(function () {
-        var playerName = $(this).attr("player-name");
-        console.log("div: " + playerName);
-        $(this).removeClass("player");
-
-        if(playerName === hero){
-          $(this).addClass("hero");
-        }
-        else{
-          $(this).addClass("enemy");
-          $("#enemies").append(this);
-        }
-      });
-    }
+    game.setHeroAndEnemies(this);
   });
 
   $("#enemies").on("click", ".enemy", function(){
-    if(game.defender !== ''){
-      console.log("You have already selected a defender");
-    }
-    else{
-      var defender = $(this).attr("player-name");
-      game.defender = game.players[defender];
-      console.log(defender);
-      $(this).addClass("defender");
-      $(this).removeClass("enemy");
-      $("#defender").append(this);
-    }
+    game.setDefender(this);
   });
 
   $("#button-attack").on("click", function(){
-      console.log("hero HP :" + game.hero.healthPoints);
-      game.hero.attack(game.defender);
-
-      console.log("hero HP :" + game.hero.healthPoints);
-
-
+    game.attack();
   });
 
-
-  // Player.prototype = {
-  // }
+  $("#button-restart").on("click", function(){
+    game.clearCharacters();
+    game.reset();
+  });
 
 });
